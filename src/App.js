7d4826537cdae0b197b987bd39ad1b1de9a7c1c4
Nikhil <vibhani.n@gmail.com>
2019-04-24 19:22:01 +0530

    Adding cube with click

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.animate = this.animate.bind(this);
+		this.objects = new Array();
 		this.addCube = this.addCube.bind(this);
 		this.initializeCamera = this.initializeCamera.bind(this);
 		this.initializeOrbits = this.initializeOrbits.bind(this);
+		this.onDocumentMouseMove = this.onDocumentMouseMove.bind(this);
+		
 	}
 
 	componentDidMount() {
@@ -18,6 +21,7 @@ class App extends Component {
 		const height = this.mount.clientHeight;
 		this.scene = new THREE.Scene();
 		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+		// this.renderer = new THREE.CanvasRenderer();
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
 		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 		this.renderer.setSize(width, height);
@@ -31,22 +35,26 @@ class App extends Component {
     var texture4 = new THREE.TextureLoader().load( require('./assests/1.jpg') );
     var texture5 = new THREE.TextureLoader().load( require('./assests/6.jpg') );
     
-		const geometry = new THREE.BoxGeometry(2, 2, 2);
+		const geometry = new THREE.BoxGeometry(1,1,1);
+
     // const material = new THREE.MeshBasicMaterial({ map: texture });
     const material = [
-      new THREE.MeshBasicMaterial( { map: texture, name:"Facebook" } ),
-      new THREE.MeshBasicMaterial( { map: texture1, name:"Facebook" } ),
-      new THREE.MeshBasicMaterial( { map: texture2, name:"Facebook" } ),
-      new THREE.MeshBasicMaterial( { map: texture3, name:"Facebook" } ),
-      new THREE.MeshBasicMaterial( { map: texture4, name:"Facebook" } ),
-      new THREE.MeshBasicMaterial( { map: texture5, name:"Facebook" } )
-  ];
-    this.cube = new THREE.Mesh(geometry, material);
-    
+      new THREE.MeshBasicMaterial( { map: texture, name:"camera" } ),
+      new THREE.MeshBasicMaterial( { map: texture1, name:"twitter" } ),
+      new THREE.MeshBasicMaterial( { map: texture2, name:"orange" } ),
+      new THREE.MeshBasicMaterial( { map: texture3, name:"google" } ),
+      new THREE.MeshBasicMaterial( { map: texture4, name:"facebook" } ),
+			new THREE.MeshBasicMaterial( { map: texture5, name:"linkedin" } )
+	];
+	  const faceMaterial = new THREE.MeshFaceMaterial( material );
+    this.cube = new THREE.Mesh(geometry, faceMaterial);
+    this.objects.push(this.cube);
     this.scene.add(this.cube);
-    
+		
+		this.renderer.domElement.addEventListener( 'dblclick', this.onDocumentMouseMove, false );
 		this.animate();
 	}
+
 	componentWillUnmount() {
 		cancelAnimationFrame(this.frameId);
 		this.mount.removeChild(this.renderer.domElement);
@@ -61,17 +69,28 @@ class App extends Component {
 		this.camera.position.y = 0;
 		this.camera.position.z = 4;
 	}
+	// animate() {
+	// 	this.frameId = window.requestAnimationFrame(this.animate);
+	// 	this.renderer.render(this.scene, this.camera);
+	// }
 	animate() {
-		this.frameId = window.requestAnimationFrame(this.animate);
-		this.renderer.render(this.scene, this.camera);
+    this.cube.rotation.x += 0.01
+    this.cube.rotation.y += 0.01
+
+    this.renderScene()
+		this.frameId = window.requestAnimationFrame(this.animate)	
 	}
+
+	renderScene() {
+    this.renderer.render(this.scene, this.camera)
+	}
+	
 	addCube(cube) {
 		this.scene.add(cube);
 	}
 
-	onDocMouseDown(event, mesh) {
-    const windowArea = event.target.getBoundingClientRect();
-    
+	onDocumentMouseMove(event) {
+		const windowArea = event.target.getBoundingClientRect();
 		const mouse3D = new THREE.Vector3(
 			(event.clientX / this.mount.width) * 2 - 1,
 			-(event.clientY / this.mount.height) * 2 + 1,
@@ -81,18 +100,57 @@ class App extends Component {
 		const raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse3D, this.camera);
     
-    let intersects = raycaster.intersectObjects(mesh);
-    alert("You have clicked.")
-		// if (intersects.length > 0) {
-		// 	const hexCode = intersects[0].object.material.color.setHex(Math.random() * 0xffffff);
-		// }
+		let intersects = raycaster.intersectObjects(this.objects);	
+		const { materialIndex } = intersects[0].face;		
+		// alert(`You have clicked on ${intersects[0].object.material[materialIndex].name}`);
+		switch(intersects[0].object.material[materialIndex].name) {
+			case 'camera':
+				window.open('https://www.instagram.com','_blank');
+			break;
+			case 'twitter':
+				window.open('https://twitter.com','_blank');
+			break;
+			case 'orange':
+				window.open('https://rss.com','_blank');
+			break;
+			case 'google':
+				window.open('http://google.com','_blank');
+			break;
+			case 'facebook':
+				window.open('https://www.facebook.com','_blank');
+			break;
+			case 'linkedin':
+				window.open('https://in.linkedin.com','_blank');
+			break;
+			
+		}
+
 	}
+
+	// onDocMouseDown(event, mesh) {
+  //   const windowArea = event.target.getBoundingClientRect();
+  //   console.log(event.dispatchConfig, windowArea)
+	// 	const mouse3D = new THREE.Vector3(
+	// 		(event.clientX / this.mount.width) * 2 - 1,
+	// 		-(event.clientY / this.mount.height) * 2 + 1,
+	// 		0.5
+  //   );
+
+	// 	const raycaster = new THREE.Raycaster();
+  //   raycaster.setFromCamera(mouse3D, this.camera);
+    
+	// 	let intersects = raycaster.intersectObjects(mesh);
+	// 	console.log("mesh", mesh)
+  //   // alert("You have clicked.")
+	// 	// if (intersects.length > 0) {
+	// 	// 	const hexCode = intersects[0].object.material.color.setHex(Math.random() * 0xffffff);
+	// 	// }
+	// }
 
 	render() {
 		return (
 			<div>
-				<div
-					onDoubleClick={e => this.onDocMouseDown(e, this.cube)}
+				<div					
 					id="boardCanvas"
 					style={{ width: '80vw', height: '40vw' }}
 					ref={mount => {
